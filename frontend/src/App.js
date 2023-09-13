2ade845d80862e0faf89604db46b422246050029
Reto <reto93@bluewin.ch>
2023-09-13 03:28:54 +0200

    creation of register cmpnt and page, routes set

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,18 @@
-import React from 'react';
-import './index.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import LoginPage from './pages/loginPage/loginPage';
+import RegisterPage from './pages/loginPage/loginRegisterPage';
+import './index.css';
 
 function App() {
     return (
-        <div className="App">
-            <LoginPage />
-        </div>
+        <Router>
+            <div className="App">
+                <Routes>
+                    <Route path="/login" element={<LoginPage />} />
+                    <Route path="/register" element={<RegisterPage />} />
+                </Routes>
+            </div>
+        </Router>
     );
 }
-
 export default App;
