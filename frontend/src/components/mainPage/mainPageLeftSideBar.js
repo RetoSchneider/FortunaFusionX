093fd1d69e8d9f2def3942234b987f329fca8a7e
Reto <reto93@bluewin.ch>
2023-10-25 02:14:01 +0200

    smooth sidebars with fancy button refactored

--- frontend/src/components/mainPage/mainPageLeftSideBar.js
+++ frontend/src/components/mainPage/mainPageLeftSideBar.js
@@ -23,37 +23,45 @@ const LeftSideBar = () => {
   return (
     <div className="relative h-[calc(100vh -120px)] border-t border-b border-gray-300">
       <div
-        className={`bg-teal-50 p-4 border-r border-gray-300 h-full transform transition-transform duration-300 ease-in-out ${
+        className={`bg-teal-50 p-4 border-r border-gray-300 h-full transform transition-all duration-700 ease-in-out shadow-md ${
           isSidebarVisible ? "w-[250px]" : "w-[40px]"
         }`}
       >
-        {isSidebarVisible && (
-          <div className="content">
-            <h2 className="font-cabin font-bold text-lg text-black mt-8 mb-4">
+        <div 
+          className={`content space-y-6 text-left transition-opacity duration-700 ease-in-out transition-max-height overflow-hidden ${
+            isSidebarVisible ? "opacity-100 max-h-[calc(100vh-120px)]" : "opacity-0 max-h-0"
+          }`}
+        >
+            <h2 className="font-cabin font-bold text-xl text-black border-b pb-2 pt-5">
               Statistic
             </h2>
-            <button className="block font-cabin font-bold text-md text-black hover:text-gray-600 transform transition-transform duration-100 hover:scale-105 active:scale-95 mb-2">
-              Latest Results
+            <button className="block w-full text-left font-cabin font-bold text-md text-black hover:text-gray-600 transform transition-transform duration-100 hover:scale-105 active:scale-95 py-2 rounded-lg shadow hover:shadow-md bg-white flex items-center pl-3">
+              <span className="mr-2">📊</span> Latest Results
             </button>
-            <button className="block font-cabin font-bold text-md text-black hover:text-gray-600 transform transition-transform duration-100 hover:scale-105 active:scale-95 mb-8">
-              Result History
+            <button className="block w-full text-left font-cabin font-bold text-md text-black hover:text-gray-600 transform transition-transform duration-100 hover:scale-105 active:scale-95 py-2 rounded-lg shadow hover:shadow-md bg-white flex items-center pl-3">
+              <span className="mr-2">📅</span> Result History
             </button>
-            <h2 className="font-cabin font-bold text-lg text-black mb-4">
+            <h2 className="font-cabin font-bold text-xl text-black border-b pb-2">
               Generators
             </h2>
-            <button className="block font-cabin font-bold text-md text-black hover:text-gray-600 transform transition-transform duration-100 hover:scale-105 active:scale-95 mb-2">
-              Random Number Generator
+            <button className="block w-full text-left font-cabin font-bold text-md text-black hover:text-gray-600 transform transition-transform duration-100 hover:scale-105 active:scale-95 py-2 rounded-lg shadow hover:shadow-md bg-white flex items-center pl-3">
+              <span className="mr-2">🎲</span> Random Number Generator
             </button>
-            <button className="block font-cabin font-bold text-md text-black hover:text-gray-600 transform transition-transform duration-100 hover:scale-105 active:scale-95 mb-2">
-              Enhanced Number Generator
+            <button className="block w-full text-left font-cabin font-bold text-md text-black hover:text-gray-600 transform transition-transform duration-100 hover:scale-105 active:scale-95 py-2 rounded-lg shadow hover:shadow-md bg-white flex items-center pl-3">
+              <span className="mr-2">🚀</span> Enhanced Number Generator
             </button>
-          </div>
-        )}
+            </div>
         <button
           onClick={() => setIsSidebarVisible(!isSidebarVisible)}
-          className="absolute top-0 right-0 mt-2 mr-2 w-6 h-6 pb-1 rounded-full bg-teal-500 hover:bg-teal-100 border border-black font-bold z-10 flex items-center justify-center text-white hover:text-black"
+          className={`absolute top-0 right-0 mt-1 mr-2 font-extrabold z-10 flex items-center justify-center transition-transform duration-500 ${
+            isSidebarVisible ? "text-red-500" : "text-blue-500"
+          } hover:text-teal-700`}
+          style={{
+            transform: isSidebarVisible ? "rotate(45deg)" : "rotate(-360deg)",
+            fontSize: '2rem'
+          }}
         >
-          {isSidebarVisible ? "-" : "+"}
+          +
         </button>
       </div>
     </div>
--- frontend/src/components/mainPage/mainPageRightSideBar.js
+++ frontend/src/components/mainPage/mainPageRightSideBar.js
@@ -23,17 +23,27 @@ const RightSideBar = () => {
   return (
     <div className="relative h-[calc(100vh -120px)] border-t border-b border-gray-300">
       <div
-        className={`bg-teal-50 p-4 border-l border-gray-300 h-full transform transition-transform duration-300 ease-in-out ${
+        className={`bg-teal-50 p-4 border-l border-gray-300 h-full transform transition-all duration-700 ease-in-out shadow-md ${
           isSidebarVisible ? "w-[250px]" : "w-[40px]"
         }`}
       >
-        {isSidebarVisible && <div className="content"></div>}
-
+        <div 
+          className={`content space-y-6 text-left transition-opacity duration-700 ease-in-out transition-max-height overflow-hidden ${
+            isSidebarVisible ? "opacity-100 max-h-[calc(100vh-120px)]" : "opacity-0 max-h-0"
+          }`}
+        >
+        </div>
         <button
           onClick={() => setIsSidebarVisible(!isSidebarVisible)}
-          className="absolute top-0 left-0 mt-2 ml-2 w-6 h-6 pb-1 rounded-full bg-teal-500 hover:bg-teal-100 border border-black font-bold z-10 flex items-center justify-center text-white hover:text-black"
+          className={`absolute top-0 left-0 mt-1 ml-2 font-extrabold z-10 flex items-center justify-center transition-transform duration-500 ${
+            isSidebarVisible ? "text-red-500" : "text-blue-500"
+          } hover:text-teal-700`}
+          style={{
+            transform: isSidebarVisible ? "rotate(-45deg)" : "rotate(360deg)",
+            fontSize: '2rem'
+          }}
         >
-          {isSidebarVisible ? "-" : "+"}
+          +
         </button>
       </div>
     </div>
